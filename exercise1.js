d962bd5c965552896fbdda7bda2505a3c01f195d
kasparas92 <kasparas.jurkevicius@euromonitor.com>
2020-05-13 18:39:20 +0300

    Delete exercise1.js

--- exercise1.js
+++
@@ -1,9 +0,0 @@
-let arr = [];
-let numbers = prompt('Enter Number: ', '12 11 13 14').split(' ');
-
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] % 2 == 0) {
-    arr.push(numbers[i]);
-  }
-}
-console.log(arr);
